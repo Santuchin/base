e9d909be06aca66c8f1beae45da55ea1a0cb2f18
Santuchin <santiago_saulino@protonmail.com>
2023-03-18 08:46:32 -0300

    moved files

--- src/c/void.h
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-char* encode(unsigned long long int number, char* digits)
-unsigned long long int decode(char* number, char* digits);
-size_t length(unsigned long long int, size_t base);
-size_t valid(char* digits, char* digits, bool leading);
-
