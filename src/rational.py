e0ed1da4090be6c63ff0c9e5a7d1fc39c861db94
Santuchin <santiago_saulino@protonmail.com>
2023-03-29 02:18:31 -0300

    small optimizations

--- src/rational.py
+++ src/rational.py
@@ -11,23 +11,24 @@ def encode(number: tuple[int, int], digits) -> tuple:
     ratper = digits[0:0]
     mods = []
     
-    while mod and not mod in mods:
+    while mod:
     
         mods.append(mod)
 
         div, mod = divmod(mod * base, den)
         ratper += digits[div:div + 1]
 
-    if mod:
+        if mod in mods:
+            
+            index = mods.index(mod)
         
-        index = mods.index(mod)
-    
-        rat = ratper[:index]
-        per = ratper[index:]
+            rat = ratper[:index]
+            per = ratper[index:]
 
-    else:
-        rat = ratper
-        per = digits[0:0]
+            return (natural.encode(nat, digits), rat, per)
+
+    rat = ratper
+    per = digits[0:0]
 
     return (natural.encode(nat, digits), rat, per)
 
@@ -37,32 +38,19 @@ def decode(number: tuple, digits) -> tuple[int, int]:
 
     base = len(digits)
 
-
     ratlen = len(rat)
 
-    if ratlen:
-        ratnum = natural.decode(rat, digits)
-
-    else:
-        ratnum = 0
-    
+    ratnum = natural.decode(rat, digits) if ratlen else 0
     pernum = natural.decode(per, digits)
 
     if pernum:
 
         perden = base ** len(per) - 1
-
         ratden = base ** ratlen
     
     else:
-    
         perden = 1
-        
-        if ratnum:
-            ratden = base ** ratlen
-
-        else:
-            ratden = 1
+        ratnum = base ** ratlen else 1
 
     num = ratnum * perden + pernum
     den = ratden * perden
